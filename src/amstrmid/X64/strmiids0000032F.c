969f24285ca698ab55412842e72120a0cc8c4754
Frankie-PellesC <f.dellacorte@tiscali.it>
2016-10-10 16:04:31 +0200

    Add files via upload

---
+++ src/amstrmid/X64/strmiids0000032F.c
@@ -0,0 +1,15 @@
+// Created file "Lib\src\amstrmid\X64\strmiids"
+
+typedef struct _GUID
+{
+    unsigned long  Data1;
+    unsigned short Data2;
+    unsigned short Data3;
+    unsigned char  Data4[8];
+} GUID;
+
+#define DEFINE_GUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
+        extern const GUID name = { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }
+
+DEFINE_GUID(CLSID_DxtJpeg, 0xde75d012, 0x7a65, 0x11d2, 0x8c, 0xea, 0x00, 0xa0, 0xc9, 0x44, 0x1e, 0x20);
+
