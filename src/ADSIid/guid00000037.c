157de56c98589289e781a76bbd0d6f45243df1ea
Frankie-PellesC <f.dellacorte@tiscali.it>
2016-10-10 15:05:48 +0200

    Add files via upload

---
+++ src/ADSIid/guid00000037.c
@@ -0,0 +1,15 @@
+// Created file "Lib\src\ADSIid\guid"
+
+typedef struct _GUID
+{
+    unsigned long  Data1;
+    unsigned short Data2;
+    unsigned short Data3;
+    unsigned char  Data4[8];
+} GUID;
+
+#define DEFINE_GUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
+        extern const GUID name = { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }
+
+DEFINE_GUID(GUID_BATTERY_DISCHARGE_FLAGS_1, 0xbcded951, 0x187b, 0x4d05, 0xbc, 0xcc, 0xf7, 0xe5, 0x19, 0x60, 0xc2, 0x58);
+
