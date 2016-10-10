75f2f1c4e459cbf9458f06e44b1b0f05a0ef962c
Frankie-PellesC <f.dellacorte@tiscali.it>
2016-10-10 16:20:38 +0200

    Add files via upload

---
+++ src/dmoguids/dmoguids00000008.c
@@ -0,0 +1,15 @@
+// Created file "Lib\src\dmoguids\dmoguids"
+
+typedef struct _GUID
+{
+    unsigned long  Data1;
+    unsigned short Data2;
+    unsigned short Data3;
+    unsigned char  Data4[8];
+} GUID;
+
+#define DEFINE_GUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
+        extern const GUID name = { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }
+
+DEFINE_GUID(GUID_POWERSCHEME_PERSONALITY, 0x245d8541, 0x3943, 0x4422, 0xb0, 0x25, 0x13, 0xa7, 0x84, 0xf6, 0x79, 0xb7);
+
