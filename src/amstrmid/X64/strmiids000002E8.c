9eb638413cae8e013fb22fa04c6ee2eb800d1130
Frankie-PellesC <f.dellacorte@tiscali.it>
2016-10-10 15:58:11 +0200

    Add files via upload

---
+++ src/amstrmid/X64/strmiids000002E8.c
@@ -0,0 +1,15 @@
+// Created file "Lib\src\amstrmid\X64\strmiids"
+
+typedef struct _GUID
+{
+    unsigned long  Data1;
+    unsigned short Data2;
+    unsigned short Data3;
+    unsigned char  Data4[8];
+} GUID;
+
+#define DEFINE_GUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
+        extern const GUID name = { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }
+
+DEFINE_GUID(MR_VIDEO_MIXER_SERVICE, 0x073cd2fc, 0x6cf4, 0x40b7, 0x88, 0x59, 0xe8, 0x95, 0x52, 0xc8, 0x41, 0xf8);
+
