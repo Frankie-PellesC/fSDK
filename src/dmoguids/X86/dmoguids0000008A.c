bab996b30cba6b09c2110cc687662e9e02248410
Frankie-PellesC <f.dellacorte@tiscali.it>
2016-10-10 16:25:05 +0200

    Add files via upload

---
+++ src/dmoguids/X86/dmoguids0000008A.c
@@ -0,0 +1,15 @@
+// Created file "Lib\src\dmoguids\X64\dmoguids"
+
+typedef struct _GUID
+{
+    unsigned long  Data1;
+    unsigned short Data2;
+    unsigned short Data3;
+    unsigned char  Data4[8];
+} GUID;
+
+#define DEFINE_GUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
+        extern const GUID name = { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }
+
+DEFINE_GUID(DMOCATEGORY_AUDIO_NOISE_SUPPRESS, 0xe07f903f, 0x62fd, 0x4e60, 0x8c, 0xdd, 0xde, 0xa7, 0x23, 0x66, 0x65, 0xb5);
+
