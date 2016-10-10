3dd7701323a8b78adbcca8668054ea069faf2588
Frankie-PellesC <f.dellacorte@tiscali.it>
2016-10-10 15:27:51 +0200

    Add files via upload

---
+++ src/ADSIid/X64/guid00000088.c
@@ -0,0 +1,15 @@
+// Created file "Lib\src\ADSIid\X64\guid"
+
+typedef struct _GUID
+{
+    unsigned long  Data1;
+    unsigned short Data2;
+    unsigned short Data3;
+    unsigned char  Data4[8];
+} GUID;
+
+#define DEFINE_GUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
+        extern const GUID name = { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }
+
+DEFINE_GUID(IID_IADsComputerOperations, 0xef497680, 0x1d9f, 0x11cf, 0xb1, 0xf3, 0x02, 0x60, 0x8c, 0x9e, 0x75, 0x53);
+
---
+++ src/ADSIid/X64/guid00000095.c
@@ -0,0 +1,15 @@
+// Created file "Lib\src\ADSIid\X64\guid"
+
+typedef struct _GUID
+{
+    unsigned long  Data1;
+    unsigned short Data2;
+    unsigned short Data3;
+    unsigned char  Data4[8];
+} GUID;
+
+#define DEFINE_GUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
+        extern const GUID name = { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }
+
+DEFINE_GUID(IID_IADsResource, 0x34a05b20, 0x4aab, 0x11cf, 0xae, 0x2c, 0x00, 0xaa, 0x00, 0x6e, 0xbf, 0xb9);
+
