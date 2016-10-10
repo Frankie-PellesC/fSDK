47dab85dbbe3b33d67d28f88141aa8996e9eaf6d
Frankie-PellesC <f.dellacorte@tiscali.it>
2016-10-10 15:53:48 +0200

    Add files via upload

---
+++ src/amstrmid/X64/strmiids000000E8.c
@@ -0,0 +1,15 @@
+// Created file "Lib\src\amstrmid\X64\strmiids"
+
+typedef struct _GUID
+{
+    unsigned long  Data1;
+    unsigned short Data2;
+    unsigned short Data3;
+    unsigned char  Data4[8];
+} GUID;
+
+#define DEFINE_GUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
+        extern const GUID name = { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }
+
+DEFINE_GUID(CLSID_MediaMultiplexerCategory, 0x236c9559, 0xadce, 0x4736, 0xbf, 0x72, 0xba, 0xb3, 0x4e, 0x39, 0x21, 0x96);
+
