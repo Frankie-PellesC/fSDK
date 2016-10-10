e3e6d0844571f1347f20a0c592885805e77f9043
Frankie-PellesC <f.dellacorte@tiscali.it>
2016-10-10 16:21:53 +0200

    Add files via upload

---
+++ src/dmoguids/dmoguids0000006B.c
@@ -0,0 +1,15 @@
+// Created file "Lib\src\dmoguids\dmoguids"
+
+typedef struct _GUID
+{
+    unsigned long  Data1;
+    unsigned short Data2;
+    unsigned short Data3;
+    unsigned char  Data4[8];
+} GUID;
+
+#define DEFINE_GUID(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8) \
+        extern const GUID name = { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }
+
+DEFINE_GUID(GUID_BACKGROUND_TASK_NOTIFICATION, 0xcf23f240, 0x2a54, 0x48d8, 0xb1, 0x14, 0xde, 0x15, 0x18, 0xff, 0x05, 0x2e);
+
